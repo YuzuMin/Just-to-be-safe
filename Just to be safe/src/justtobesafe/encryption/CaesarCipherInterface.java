91460565b914cc5e1ba2fde713416efe193af1fe
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-26 03:53:07 +0800

    Create CaesarCipherInterface.java

---
+++ Just to be safe/src/justtobesafe/encryption/CaesarCipherInterface.java
@@ -0,0 +1,6 @@
+package justtobesafe.encryption;
+
+public interface CaesarCipherInterface {
+    public String cc_encrypt(String message);       //Caesar Cipher Encryption
+    public String cc_decrypt(String message);       //Caesar Cipher Decryption
+}
