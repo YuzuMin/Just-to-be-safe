cbd232f302fa27af6352692dff80cfdc99103e50
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-26 03:54:15 +0800

    Update Main.java

--- Just to be safe/src/justtobesafe/Main.java
+++ Just to be safe/src/justtobesafe/Main.java
@@ -1,43 +1,26 @@
 package justtobesafe;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.stage.StageStyle;
-
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-import java.util.logging.Level;
-//import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
 
-
-    @FXML
-    private TextField password;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login/login_activity.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login/login_activity.fxml"));
         primaryStage.setTitle("Just to be safe");
-        Image image = new Image("file:/icon.png");
         primaryStage.getIcons().add(new Image("/resources/icon_inverse.png"));
         primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
-    }
-
-    private void closeStage() {
-        ((Stage) password.getScene().getWindow()).close();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 
 }
