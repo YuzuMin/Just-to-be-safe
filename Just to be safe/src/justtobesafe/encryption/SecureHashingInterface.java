115008ba649f644acd0b2ff80d5e421ef8353dcf
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-26 03:54:05 +0800

    Create SecureHashingInterface.java

---
+++ Just to be safe/src/justtobesafe/encryption/SecureHashingInterface.java
@@ -0,0 +1,11 @@
+package justtobesafe.encryption;
+
+import java.security.MessageDigest;
+
+public interface SecureHashingInterface {
+
+    public String sha1( String base);       //Secure one-way 160 Bit hash
+    public String sha256( String base);     //Secure one-way 256 Bit hash
+    public String sha384( String base);     //Secure one-way 384 Bit hash
+    public String sha512( String base);     //Secure one-way 512 Bit hash
+}
