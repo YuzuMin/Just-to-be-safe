1541f0128500b77f1bb74c17a074791601b487a4
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-27 23:53:52 +0800

    Update MessageDigestInterface.java

--- Just to be safe/src/justtobesafe/encryption/MessageDigestInterface.java
+++ Just to be safe/src/justtobesafe/encryption/MessageDigestInterface.java
@@ -1,8 +1,6 @@
 package justtobesafe.encryption;
 
 public interface MessageDigestInterface {
-    public String md2( String base);       //Secure one-way 160 Bit hash
-    public String md4( String base);     //Secure one-way 256 Bit hash
+    public String md2( String base);       //Secure one-way 160 Bit has
     public String md5( String base);     //Secure one-way 384 Bit hash
-    public String md6( String base);     //Secure one-way 512 Bit hash
 }
