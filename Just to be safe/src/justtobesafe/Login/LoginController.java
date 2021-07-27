b71435c3c7ceed52a4618a831b6dd674bfd81e46
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-27 08:46:39 +0800

    Update LoginController.java

--- Just to be safe/src/justtobesafe/Login/LoginController.java
+++ Just to be safe/src/justtobesafe/Login/LoginController.java
@@ -1,96 +1,88 @@
 package justtobesafe.login;
 
+// JAVA FX Imports
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-
-import java.awt.event.MouseEvent;
-import java.util.prefs.Preferences;
-
+import javafx.scene.input.MouseEvent;
+// JAVA imports
+import java.io.IOException;
+// Other imports
 import justtobesafe.encryption.EncryptionHandler;
 
-import javax.swing.*;
 
 
 public class LoginController {
-    public CheckBox pswd_checkbox;
     @FXML
     private PasswordField password;
     @FXML
     private Label pswd_warning;
 
-
-
-    Preferences preference;
     EncryptionHandler encryptionHandler=new EncryptionHandler();
     String file = "src/resources/passwd/passwd";
 
-
-
-    @FXML
-    private void handleLoginButtonAction(MouseEvent event) {
-        //String pword = DigestUtils.shaHex(password.getText());
-
-        //if (pword.equals(preference.getPassword())) {
-            //closeStage();
-            //loadMain();
-            //LOGGER.log(PlatformLogger.Level.INFO, "User successfully logged in {}", uname);
-        //}
-        //else {
-        password.getStyleClass().add("wrong-credentials");
-        //}
-    }
-
     //Login Related Functions
+    //Authenticate password when user presses "Enter" key after typing in password field
     public void EnterKeyPress(KeyEvent keyEvent) {
         if (keyEvent.getCode().equals(KeyCode.ENTER)) {
             authenticatePSWD();
         }
     }
-
-    public void LoginButtonOnclick(javafx.scene.input.MouseEvent mouseEvent) {
+    //Authenticate password when user presses on-screen Login button
+    public void LoginButtonOnclick(MouseEvent mouseEvent) {
         authenticatePSWD();
     }
-
+    //Authenticate password
     public void authenticatePSWD(){
-        String pswd=encryptionHandler.sha512(password.getText());             //User Entered password
-        String passwd=encryptionHandler.cc_decrypt(encryptionHandler.readPasswordFile(file));      //Program stored password
+        String passwd=encryptionHandler.readPasswordFile(file);         //retrieve data from passwd
+        String pswd=password.getText();                                 //User Entered password
+        pswd=encryptionHandler.cc_encrypt(pswd);                        //Caesar Cipher Encrypt password
+        pswd=encryptionHandler.sha512(pswd);                            //Hash Encrypted password using Secure Hashing Algorithm 512
+        pswd=encryptionHandler.cc_encrypt(pswd);                        //Caesar Cipher Encrypt Hashed password
 
+        //System.out.println(pswd);
+        //Compare password string with string from passwd file
         if(pswd.equals(passwd)){
-            pswd_warning.setText("Access Granted");
-            password.setText("");
+            //pswd_warning.setText("Access Granted");
+            //password.setText("");
+            closeStage();
+            loadHomeMenu();
         }else{
             pswd_warning.setText("Access Denied");
             password.setText("");
         }
     }
 
+
     //MISC functions
-    public void PSWDFieldOnclick(javafx.scene.input.MouseEvent mouseEvent) {
+
+    public void PSWDFieldOnclick(MouseEvent mouseEvent) {
         pswd_warning.setText("");
         password.setPromptText("Enter Password");
     }
-    //private void closeStage() {
-        //close();
-    //}
 
-    /*void loadMain() {
-        try {
-            Parent parent = FXMLLoader.load(getClass().getResource("/library/assistant/ui/main/main.fxml"));
-            Stage stage = new Stage(StageStyle.DECORATED);
-            stage.setTitle("Library Assistant");
-            stage.setScene(new Scene(parent));
-            stage.show();
-            LibraryAssistantUtil.setStageIcon(stage);
-        }
-        catch (IOException ex) {
-            LOGGER.log(Level.ERROR, "{}", ex);
-        }
+    private void closeStage() {
+        ((Stage) password.getScene().getWindow()).close();
     }
 
-     */
-
+    private void loadHomeMenu() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/justtobesafe/homemenu/homemenu_activity.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Just to be safe");
+            primaryStage.getIcons().add(new Image("/resources/icon_inverse.png"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        catch (IOException ex) {
+        }
+    }
 }
