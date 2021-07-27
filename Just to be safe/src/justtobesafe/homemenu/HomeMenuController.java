eef94256930bfb2b615f5757d97902a295d6017d
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-27 09:23:03 +0800

    Update HomeMenuController.java

--- Just to be safe/src/justtobesafe/homemenu/HomeMenuController.java
+++ Just to be safe/src/justtobesafe/homemenu/HomeMenuController.java
@@ -1,16 +1,11 @@
 package justtobesafe.homemenu;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import justtobesafe.encryption.EncryptionHandler;
 
-import java.io.IOException;
+import justtobesafe.activity.ActivityHandler;
 
 public class HomeMenuController {
     private String accountMenu_location="/justtobesafe/accountmenu/accountmenu_activity.fxml";
@@ -18,47 +13,24 @@ public class HomeMenuController {
     private String settingsMenu_location="/justtobesafe/settingsmenu/settingsmenu_activity.fxml";
 
     EncryptionHandler encryptionHandler=new EncryptionHandler();
+    ActivityHandler activityHandler=new ActivityHandler();
 
     @FXML
     AnchorPane HomeMenu;
 
-
-
-
-
-
-
-    private void loadActivity(String activityLocation) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource(activityLocation));
-            Stage primaryStage = new Stage();
-            primaryStage.setTitle("Just to be safe");
-            primaryStage.getIcons().add(new Image("/resources/icon_inverse.png"));
-            primaryStage.setScene(new Scene(root));
-            primaryStage.setResizable(false);
-            primaryStage.show();
-        }
-        catch (IOException ex) {
-        }
-    }
-    //Close activity
-    private void closeStage() {
-        ((Stage) HomeMenu.getScene().getWindow()).close();
-    }
-
     //Click Accounts & Passwords button
     public void onAccountButtonClicked(MouseEvent mouseEvent) {
-        closeStage();
-        loadActivity(accountMenu_location);
+        activityHandler.closeStage(HomeMenu);
+        activityHandler.loadActivity(accountMenu_location);
     }
     //Click Settings button
     public void onSettingsButtonClicked(MouseEvent mouseEvent) {
-        closeStage();
-        loadActivity(settingsMenu_location);
+        activityHandler.closeStage(HomeMenu);
+        activityHandler.loadActivity(settingsMenu_location);
     }
     //Click Credit Cards button
     public void onCardButtonClicked(MouseEvent mouseEvent) {
-        closeStage();
-        loadActivity(cardMenu_location);
+        activityHandler.closeStage(HomeMenu);
+        activityHandler.loadActivity(cardMenu_location);
     }
 }
