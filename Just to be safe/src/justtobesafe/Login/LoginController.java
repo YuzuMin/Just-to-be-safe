93685de12e7b76e4f672e726f7ef5ac97e335270
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-27 22:29:14 +0800

    Update LoginController.java

--- Just to be safe/src/justtobesafe/Login/LoginController.java
+++ Just to be safe/src/justtobesafe/Login/LoginController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-// JAVA imports
 // Other imports
 import justtobesafe.activity.ActivityHandler;
 import justtobesafe.encryption.EncryptionHandler;
