6eedc35c5cae5ccbea1a628fba6630bb0a1da47f
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-28 02:08:30 +0800

    Update SettingsMenuController.java

--- Just to be safe/src/justtobesafe/settingsmenu/SettingsMenuController.java
+++ Just to be safe/src/justtobesafe/settingsmenu/SettingsMenuController.java
@@ -1,4 +1,8 @@
 package justtobesafe.settingsmenu;
 
+import javafx.scene.input.MouseEvent;
+
 public class SettingsMenuController {
+    public void onpasswdResetButtonClicked(MouseEvent mouseEvent) {
+    }
 }
