b083c9af8673608829a07c42bc0ea013a8bb1788
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-28 03:38:27 +0800

    Create ActivityLocations.java

---
+++ Just to be safe/src/justtobesafe/activity/ActivityLocations.java
@@ -0,0 +1,11 @@
+package justtobesafe.activity;
+
+public class ActivityLocations {
+
+    public static final String homeMenu="/justtobesafe/homemenu/homemenu_activity.fxml";
+    public static final String loginActivity="/justtobesafe/login/login_activity.fxml";
+    public static final String accountMenu="/justtobesafe/accountmenu/accountmenu_activity.fxml";
+    public static final String cardMenu="/justtobesafe/cardmenu/cardmenu_activity.fxml";
+    public static final String settingsMenu="/justtobesafe/settingsmenu/settingsmenu_activity.fxml";
+
+}
