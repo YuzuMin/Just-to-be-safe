ad32783ed2d5826570ab7b81753f877b8f51a797
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-29 03:03:20 +0800

    Update AccountMenuController.java

--- Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
+++ Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
@@ -1,11 +1,34 @@
 package justtobesafe.accountmenu;
 
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import justtobesafe.activity.ActivityHandler;
+import justtobesafe.activity.ActivityLocations;
 
 public class AccountMenuController {
-    public void onHomeButtonClicked(MouseEvent mouseEvent) {
+    ActivityHandler activityHandler = new ActivityHandler();
+
+    @FXML
+    private AnchorPane AccountMenu;
+    
+    
+    public void onLogoutButtonClicked(MouseEvent mouseEvent) {
+        activityHandler.closeStage(AccountMenu);
+        activityHandler.loadActivity(ActivityLocations.loginActivity);
     }
 
-    public void onLogoutButtonClicked(MouseEvent mouseEvent) {
+    public void onHomeButtonClicked(MouseEvent mouseEvent) {
+        activityHandler.closeStage(AccountMenu);
+        activityHandler.loadActivity(ActivityLocations.homeMenu);
+    }
+
+    public void onSetButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onClearButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onCopyButtonClicked(MouseEvent mouseEvent) {
     }
 }
