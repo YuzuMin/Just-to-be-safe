6c3b0437282b90c79c2ff2fe409c8ecd342ecb2a
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-28 14:26:53 +0800

    Update CaesarCipherInterface.java

--- Just to be safe/src/justtobesafe/encryption/CaesarCipherInterface.java
+++ Just to be safe/src/justtobesafe/encryption/CaesarCipherInterface.java
@@ -1,6 +1,6 @@
 package justtobesafe.encryption;
 
 public interface CaesarCipherInterface {
-    public String cc_encrypt(String message);       //Caesar Cipher Encryption
-    public String cc_decrypt(String message);       //Caesar Cipher Decryption
+    public String cc_encrypt(String message,int shift,int shift2);       //Caesar Cipher Encryption
+    public String cc_decrypt(String message,int shift,int shift2);       //Caesar Cipher Decryption
 }
