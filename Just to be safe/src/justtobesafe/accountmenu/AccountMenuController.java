201fe59e403e6dd9ce143d371442c53802bc4367
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-29 02:41:29 +0800

    Update AccountMenuController.java

--- Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
+++ Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
@@ -1,4 +1,11 @@
 package justtobesafe.accountmenu;
 
+import javafx.scene.input.MouseEvent;
+
 public class AccountMenuController {
+    public void onHomeButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onLogoutButtonClicked(MouseEvent mouseEvent) {
+    }
 }
