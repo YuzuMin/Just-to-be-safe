b921ba3e10f3dd39ebc2904c8f62ea3923190771
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-28 15:44:53 +0800

    Update CardMenuController.java

--- Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
+++ Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
@@ -1,4 +1,33 @@
 package justtobesafe.cardmenu;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import justtobesafe.activity.ActivityHandler;
+import justtobesafe.activity.ActivityLocations;
+
 public class CardMenuController {
+    ActivityHandler activityHandler = new ActivityHandler();
+
+    @FXML private AnchorPane CardMenu;
+
+    //Click Logout Button
+    public void onLogoutButtonClicked(MouseEvent mouseEvent) {
+        activityHandler.closeStage(CardMenu);
+        activityHandler.loadActivity(ActivityLocations.loginActivity);
+    }
+
+    public void onHomeButtonClicked(MouseEvent mouseEvent) {
+        activityHandler.closeStage(CardMenu);
+        activityHandler.loadActivity(ActivityLocations.homeMenu);
+    }
+
+    public void onCopyButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onClearButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onSetButtonClicked(MouseEvent mouseEvent) {
+    }
 }
