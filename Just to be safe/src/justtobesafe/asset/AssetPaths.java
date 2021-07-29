c75382005eac571dac967a82739d4bc5e90d014e
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 04:37:21 +0800

    Update AssetPaths.java

--- Just to be safe/src/justtobesafe/asset/AssetPaths.java
+++ Just to be safe/src/justtobesafe/asset/AssetPaths.java
@@ -4,4 +4,6 @@ public class AssetPaths {
     public static final String icon = "/resources/icon_inverse.png";
     public static final String title = "Just to be safe";
     public static final String passwd = "src/resources/passwd/passwd";
+    public static final String decoyIcon = "/resources/chrome.png";
+    public static final String decoyTitle = "Google Chrome";
 }
