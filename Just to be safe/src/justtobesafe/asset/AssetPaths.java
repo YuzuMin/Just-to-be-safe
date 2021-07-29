f3a908d293a0bf9f6feefd4b0cf040c0bc84dc4c
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 03:33:15 +0800

    Create AssetPaths.java

---
+++ Just to be safe/src/justtobesafe/asset/AssetPaths.java
@@ -0,0 +1,7 @@
+package justtobesafe.asset;
+
+public class AssetPaths {
+    public static final String icon = "/resources/icon_inverse.png";
+    public static final String title = "Just to be safe";
+    public static final String passwd = "src/resources/passwd/passwd";
+}
