368ccd0a098aacdf9beda16cf11ea904b211566c
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 22:15:41 +0800

    Delete CsvInterface.java

--- Just to be safe/src/justtobesafe/reader/CsvInterface.java
+++
@@ -1,7 +0,0 @@
-package justtobesafe.reader;
-
-public interface CsvInterface {
-    public void readCsvFile(String location);
-    public void writeCsvFile(String location);
-    public void deleteCSVFile(String location);
-}
