464befa12dfb052f80635a56ef05059d1c1094e8
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-31 01:02:49 +0800

    Update AccountMenuController.java

--- Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
+++ Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
@@ -1,6 +1,7 @@
 package justtobesafe.accountmenu;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -8,13 +9,18 @@ import javafx.scene.layout.AnchorPane;
 import justtobesafe.activity.ActivityHandler;
 import justtobesafe.activity.ActivityPaths;
 import justtobesafe.asset.AssetPaths;
+import justtobesafe.data.DataHandler;
 
 public class AccountMenuController {
     ActivityHandler activityHandler = new ActivityHandler();
+    DataHandler dataHandler = new DataHandler();
 
-    @FXML
-    private AnchorPane AccountMenu;
-    
+    @FXML private AnchorPane AccountMenu;
+    @FXML private TextField site_field;
+    @FXML private TextField link_field;
+    @FXML private TextField email_field;
+    @FXML private TextField pswd_field;
+
     
     public void onLogoutButtonClicked(MouseEvent mouseEvent) {
         activityHandler.closeStage(AccountMenu);
@@ -27,6 +33,16 @@ public class AccountMenuController {
     }
 
     public void onSetButtonClicked(MouseEvent mouseEvent) {
+
+
+        String Site=site_field.getText()+",";
+        String Link=link_field.getText()+",";
+        String Email=email_field.getText()+",";
+        String Password=pswd_field.getText();
+
+        String data=Site+Link+Email+Password;
+
+        dataHandler.writeCsvFile(AssetPaths.acctCSV,data);
     }
 
     public void onClearButtonClicked(MouseEvent mouseEvent) {
