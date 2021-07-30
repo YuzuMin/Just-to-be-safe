58af482a166649d1c67568ccd68763e14ee66b39
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 23:58:37 +0800

    Update CsvInterface.java

--- Just to be safe/src/justtobesafe/data/CsvInterface.java
+++ Just to be safe/src/justtobesafe/data/CsvInterface.java
@@ -1,7 +1,7 @@
 package justtobesafe.data;
 
 public interface CsvInterface {
-    public void readCsvFile(String location);
-    public void writeCsvFile(String location);
+    public String readCsvFile(String file);
+    public void writeCsvFile(String file,String inputString);
     public void deleteCSVFile(String location);
 }
