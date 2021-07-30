69b295fea3989ed0b508f5f85f977ef9df72d22b
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 20:39:50 +0800

    Update FileReader.java

--- Just to be safe/src/justtobesafe/reader/FileReader.java
+++ Just to be safe/src/justtobesafe/reader/FileReader.java
@@ -1,4 +1,37 @@
 package justtobesafe.reader;
 
-public class FileReader {
+import java.io.*;
+
+public class FileReader implements FileInterface {
+
+    @Override
+    public String readFile(String file) {
+        String currentLine=""; //Init value
+        try (BufferedReader reader = new BufferedReader
+                (new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+            currentLine = reader.readLine();    //Obtain value from
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            //return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return currentLine;
+    }
+
+    @Override
+    public void writeFile(String file,String inputString) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            bufferedWriter.write(inputString);
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
