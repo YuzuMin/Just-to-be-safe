bb1c0c295133b87e043b8652f585d69e3b843467
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-30 14:11:17 +0800

    Update CardMenuController.java

--- Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
+++ Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
@@ -3,6 +3,7 @@ package justtobesafe.cardmenu;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -42,5 +43,21 @@ public class CardMenuController {
 
     public void cardnumOnKeyTyped(KeyEvent keyEvent) {
 
+    }
+
+    public void onFunctionKeyPress(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ESCAPE)) {
+            activityHandler.loadActivity(ActivityPaths.decoyActivity,AssetPaths.decoyTitle,AssetPaths.decoyIcon);
+            activityHandler.closeStage(CardMenu);
+        }else if (keyEvent.getCode().equals(KeyCode.F1)) {
+            activityHandler.loadActivity(ActivityPaths.homeMenu, AssetPaths.title, AssetPaths.icon);
+            activityHandler.closeStage(CardMenu);
+        }else if (keyEvent.getCode().equals(KeyCode.F3)) {
+            activityHandler.loadActivity(ActivityPaths.accountMenu,AssetPaths.title,AssetPaths.icon);
+            activityHandler.closeStage(CardMenu);
+        }else if (keyEvent.getCode().equals(KeyCode.F4)) {
+            activityHandler.loadActivity(ActivityPaths.settingsMenu,AssetPaths.title,AssetPaths.icon);
+            activityHandler.closeStage(CardMenu);
+        }
     }
 }
