6aaf71bc7ec1f90fd92bd2d4b836d3103f7abcdd
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-07-31 19:34:38 +0800

    Update Account.java

--- Just to be safe/src/justtobesafe/data/Account.java
+++ Just to be safe/src/justtobesafe/data/Account.java
@@ -1,6 +1,37 @@
 package justtobesafe.data;
 
 public class Account extends DataObject{
-    String emailoruser;
-    String password;
+    private String email;
+    private String password;
+
+    public String getName(){
+        return DisplayName;
+    }
+    public void setName(String Name){
+        this.DisplayName = Name;
+    }
+
+    public String getLink(){
+        return UniqueIdentifier;
+    }
+
+    public void setLink(String Link){
+        UniqueIdentifier=Link;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
