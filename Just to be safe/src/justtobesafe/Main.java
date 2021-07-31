25c080b8be9a2bd05af812ef36091d65764fadd1
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 05:45:07 +0800

    Update Main.java

--- Just to be safe/src/justtobesafe/Main.java
+++ Just to be safe/src/justtobesafe/Main.java
@@ -7,17 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
+import justtobesafe.activity.ActivityHandler;
+import justtobesafe.activity.ActivityPaths;
+import justtobesafe.asset.AssetPaths;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login/login_activity.fxml"));
-        primaryStage.setTitle("Just to be safe");
-        primaryStage.getIcons().add(new Image("/resources/icon_inverse.png"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        ActivityHandler activityHandler = new ActivityHandler();
+        activityHandler.loadActivity(ActivityPaths.loginActivity, AssetPaths.title, AssetPaths.icon);
     }
 
     public static void main(String[] args) {
