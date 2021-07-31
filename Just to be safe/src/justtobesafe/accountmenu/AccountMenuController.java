6c1a54900cf9fdaaad1866e5a25d5717b5d147a9
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 05:44:58 +0800

    Update AccountMenuController.java

--- Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
+++ Just to be safe/src/justtobesafe/accountmenu/AccountMenuController.java
@@ -1,9 +1,16 @@
 package justtobesafe.accountmenu;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -17,9 +24,11 @@ import justtobesafe.data.DataHandler;
 import justtobesafe.encryption.EncryptionHandler;
 import justtobesafe.toast.Toast;
 
+import java.net.URL;
 import java.util.LinkedList;
+import java.util.ResourceBundle;
 
-public class AccountMenuController {
+public class AccountMenuController implements Initializable {
     EncryptionHandler encryptionHandler = new EncryptionHandler();
     ActivityHandler activityHandler = new ActivityHandler();
     DataHandler dataHandler = new DataHandler();
@@ -33,12 +42,32 @@ public class AccountMenuController {
     @FXML private Label warning2;
     @FXML private ListView<String> accountView;
 
+    int position;
+
     LinkedList<Account> accountList = new LinkedList<Account>();
-    LinkedList<String> accountListView = new LinkedList<String>();
+    //LinkedList<String> accountListView = new LinkedList<String>();
 
     //ListView<String> accountView = new ListView<String>();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayAccounts();
 
-    
+        accountView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                int value =Character.getNumericValue(newValue.charAt(0));
+                value--;
+                position=value;
+                System.out.println(value);
+                site_field.setText(accountList.get(value).getName());
+                link_field.setText(accountList.get(value).getLink());
+                email_field.setText(accountList.get(value).getEmail());
+                pswd_field.setText(accountList.get(value).getPassword());
+            }
+        });
+    }
+
+
     public void onLogoutButtonClicked(MouseEvent mouseEvent) {
         activityHandler.closeStage(AccountMenu);
         activityHandler.loadActivity(ActivityPaths.loginActivity, AssetPaths.title, AssetPaths.icon);
@@ -89,12 +118,13 @@ public class AccountMenuController {
 
             onClearButtonClicked(mouseEvent);
             Toast.makeText(((Stage) AccountMenu.getScene().getWindow()),"Account Added Successfully",500,1000,500);
+
+            activityHandler.loadActivity(ActivityPaths.accountMenu, AssetPaths.title, AssetPaths.icon);
+            activityHandler.closeStage(AccountMenu);
         }
     }
 
     public void onClearButtonClicked(MouseEvent mouseEvent) {
-        displayAccounts();
-
         site_field.setText("");
         link_field.setText("");
         email_field.setText("");
@@ -103,25 +133,25 @@ public class AccountMenuController {
 
     private void displayAccounts(){
         LinkedList<String> list = dataHandler.readCsvFile(AssetPaths.acctCSV);
+        accountList.clear();
+        accountView.getItems().clear();
+
         for(int i=0; i<list.size();i++){
             String[] EAX = list.get(i).split(",");
 
-            EAX[0]=encryptionHandler.cc_decrypt(EAX[0],69,420);
-            EAX[1]=encryptionHandler.cc_decrypt(EAX[1],70,421);
-            EAX[2]=encryptionHandler.cc_decrypt(EAX[2],71,422);
-            EAX[3]=encryptionHandler.cc_decrypt(EAX[3],72,423);
+            EAX[0]=encryptionHandler.cc_decrypt(EAX[0],69,420);     //Decrypt NAME
+            EAX[1]=encryptionHandler.cc_decrypt(EAX[1],70,421);     //Decrypt LINK
+            EAX[2]=encryptionHandler.cc_decrypt(EAX[2],71,422);     //Decrypt Email
+            EAX[3]=encryptionHandler.cc_decrypt(EAX[3],72,423);     //Decrypt Password
 
-            Account account = new Account();
-            account.setName(EAX[0]);
-            account.setLink(EAX[1]);
-            account.setEmail(EAX[2]);
-            account.setPassword(EAX[3]);
-
+            Account account = new Account(EAX[0],EAX[1],EAX[2],EAX[3]);
+            String EBX="";
+            EBX=(i+1)+". "+EAX[0];
             accountList.add(account);
-            accountListView.add(EAX[0]);
-            accountView.getItems().add(EAX[0]);
-        }
 
+            accountView.getItems().add(EBX);
+        }
+        /*
         for(int i=0; i<accountList.size();i++){
             Account acc = accountList.get(i);
             System.out.println(acc.getName());
@@ -129,11 +159,11 @@ public class AccountMenuController {
             System.out.println(acc.getEmail());
             System.out.println(acc.getPassword());
         }
-
         for(int i=0; i<accountListView.size();i++){
             String BX = accountListView.get(i);
             System.out.println(BX);
         }
+         */
     }
 
     public void onFunctionKeyPress(KeyEvent keyEvent) {
