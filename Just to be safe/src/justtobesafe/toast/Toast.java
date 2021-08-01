dc4b1c2e594c8352ee3d9d47b4e5b2bd7a4adfbd
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 19:44:11 +0800

    Update Toast.java

--- Just to be safe/src/justtobesafe/toast/Toast.java
+++ Just to be safe/src/justtobesafe/toast/Toast.java
@@ -25,7 +25,7 @@ public final class Toast
 
         Text text = new Text(toastMsg);
         text.setFont(Font.font("Verdana", 12));
-        text.setFill(Color.GREEN);
+        text.setFill(Color.GREENYELLOW);
 
         StackPane root = new StackPane(text);
         root.setStyle("-fx-background-radius: 20; -fx-background-color: rgba(0, 0, 0, 0.2); -fx-padding: 10px;");
