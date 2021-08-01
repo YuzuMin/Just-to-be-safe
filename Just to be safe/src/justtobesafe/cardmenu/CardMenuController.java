e852556405b1b3c047e07ee417b192bde0a29280
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 21:41:42 +0800

    Update CardMenuController.java

--- Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
+++ Just to be safe/src/justtobesafe/cardmenu/CardMenuController.java
@@ -2,6 +2,7 @@ package justtobesafe.cardmenu;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -10,15 +11,33 @@ import javafx.scene.layout.AnchorPane;
 import justtobesafe.activity.ActivityHandler;
 import justtobesafe.activity.ActivityPaths;
 import justtobesafe.asset.AssetPaths;
+import justtobesafe.data.Account;
+import justtobesafe.data.Card;
+import justtobesafe.data.DataHandler;
+import justtobesafe.encryption.EncryptionHandler;
+
+import java.util.LinkedList;
 
 public class CardMenuController {
+    EncryptionHandler encryptionHandler = new EncryptionHandler();
     ActivityHandler activityHandler = new ActivityHandler();
+    DataHandler dataHandler = new DataHandler();
 
     @FXML private AnchorPane CardMenu;
+    @FXML private TextField cardName_field;
     @FXML private TextField cardNum_field;
+    @FXML private TextField cvv_field;
+    @FXML private TextField expiry_field;
+    @FXML private TextField cardholder_field;
+    @FXML private ListView<String> cardView;
     @FXML private Button setBtn;
     @FXML private Button clearBtn;
 
+    int position;
+    boolean deleteBtnIsActive=false;
+
+    LinkedList<Card> cardList = new LinkedList<Card>();
+    LinkedList<String> cardEncryptedStringList = new LinkedList<String>();
 
     //Click Logout Button
     public void onLogoutButtonClicked(MouseEvent mouseEvent) {
@@ -43,6 +62,30 @@ public class CardMenuController {
 
     public void cardnumOnKeyTyped(KeyEvent keyEvent) {
 
+    }
+
+    private void displayAccounts(){
+        LinkedList<String> list = dataHandler.readCsvFile(AssetPaths.acctCSV);
+        cardEncryptedStringList.clear();
+        cardEncryptedStringList =list;
+        cardList.clear();
+        cardView.getItems().clear();
+
+        for(int i=0; i<list.size();i++){
+            String[] EAX = list.get(i).split(",");
+
+            EAX[0]=encryptionHandler.cc_decrypt(EAX[0],69,420);     //Decrypt NAME
+            EAX[1]=encryptionHandler.cc_decrypt(EAX[1],70,421);     //Decrypt LINK
+            EAX[2]=encryptionHandler.cc_decrypt(EAX[2],71,422);     //Decrypt Email
+            EAX[3]=encryptionHandler.cc_decrypt(EAX[3],72,423);     //Decrypt Password
+
+            Card card = new Card(EAX[0],EAX[1],EAX[2],EAX[3]);
+            String EBX="";
+            EBX=(i+1)+". "+EAX[0];
+            cardList.add(card);
+
+            cardView.getItems().add(EBX);
+        }
     }
 
     public void onFunctionKeyPress(KeyEvent keyEvent) {
