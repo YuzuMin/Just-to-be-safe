09a1aae0dd5c4d6c688618bd6ec7841cb56a4ecd
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 17:53:38 +0800

    Update AlertPopup.java

--- Just to be safe/src/justtobesafe/alert/AlertPopup.java
+++ Just to be safe/src/justtobesafe/alert/AlertPopup.java
@@ -6,14 +6,15 @@ import javafx.scene.control.ButtonType;
 import java.util.Optional;
 
 public class AlertPopup {
-    public static boolean confirmation(String displayText, String displayTitle){
+    public static boolean confirmation(String displayText,String smallText, String displayTitle){
         boolean retValue=false;
         Alert alert =
-                new Alert(Alert.AlertType.WARNING,
-                        displayText,
+                new Alert(Alert.AlertType.CONFIRMATION,
+                        smallText,
                         ButtonType.YES,
                         ButtonType.NO);
         alert.setTitle(displayTitle);
+        alert.setHeaderText(displayText);
         Optional<ButtonType> result = alert.showAndWait();
 
         if (result.get() == ButtonType.YES) {
