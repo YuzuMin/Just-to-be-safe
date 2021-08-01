01e189f5ba886a0404316befbc58a2d81fbf2d79
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-02 01:49:55 +0800

    Update AssetPaths.java

--- Just to be safe/src/justtobesafe/asset/AssetPaths.java
+++ Just to be safe/src/justtobesafe/asset/AssetPaths.java
@@ -2,11 +2,11 @@ package justtobesafe.asset;
 
 public class AssetPaths {
     //Program Activity
-    public static final String icon = "/resources/icon_inverse.png";
+    public static final String icon = "/resources/drawable/icon_inverse.png";
     public static final String title = "Just to be safe";
     public static final String passwd = "src/resources/passwd/passwd";
     //Decoy Activity
-    public static final String decoyIcon = "/resources/chrome.png";
+    public static final String decoyIcon = "/resources/drawable/chrome.png";
     public static final String decoyTitle = "Google Chrome";
     //Program CSV
     public static final String cardCSV = "src/resources/database/card.csv";
