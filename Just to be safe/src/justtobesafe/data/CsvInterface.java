a82987c848a226c7fe9941ac4646ca8b5defaf9d
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-01 17:53:43 +0800

    Update CsvInterface.java

--- Just to be safe/src/justtobesafe/data/CsvInterface.java
+++ Just to be safe/src/justtobesafe/data/CsvInterface.java
@@ -5,5 +5,5 @@ import java.util.LinkedList;
 public interface CsvInterface {
     public LinkedList<String> readCsvFile(String file);
     public void writeCsvFile(String file,String inputString);
-    public void deleteCSVFile(String location);
+    public void deleteCSVFile(String file,LinkedList<String> inputlist);
 }
