85635dce19fcabaa922b64b5ac36fb2634bc1bb8
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-03 00:29:41 +0800

    Update DecoyController.java

--- Just to be safe/src/justtobesafe/decoy/DecoyController.java
+++ Just to be safe/src/justtobesafe/decoy/DecoyController.java
@@ -1,18 +1,19 @@
 package justtobesafe.decoy;
 
+// JavaFX Imports
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-
+// Program Imports
+import justtobesafe.activity.ActivityHandler;
+import justtobesafe.activity.ActivityPaths;
+import justtobesafe.asset.AssetPaths;
+// Java Imports
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-
-import justtobesafe.activity.ActivityHandler;
-import justtobesafe.activity.ActivityPaths;
-import justtobesafe.asset.AssetPaths;
 
 public class DecoyController {
     @FXML AnchorPane DecoyMenu;
