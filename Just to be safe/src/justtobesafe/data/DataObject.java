df87229c020c5a7e1d11e13bd1bb36785618e138
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-03 00:29:38 +0800

    Update DataObject.java

--- Just to be safe/src/justtobesafe/data/DataObject.java
+++ Just to be safe/src/justtobesafe/data/DataObject.java
@@ -1,9 +1,6 @@
 package justtobesafe.data;
 
-import java.util.UUID;
-
 public abstract class DataObject {
-    //protected UUID uuid;
     protected String UniqueIdentifier;
     protected String DisplayName;
 }
