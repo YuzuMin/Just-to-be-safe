f8ae929a87db42bcaecf59ac6d17ea62004d1dc5
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-03 11:47:32 +0800

    Update Main.java

--- Just to be safe/src/justtobesafe/Main.java
+++ Just to be safe/src/justtobesafe/Main.java
@@ -1,22 +1,9 @@
 package justtobesafe;
 
-// JavaFX Imports
-import javafx.application.Application;
-import javafx.stage.Stage;
-// Program imports
-import justtobesafe.activity.ActivityHandler;
-import justtobesafe.activity.ActivityPaths;
-import justtobesafe.asset.AssetPaths;
+import justtobesafe.launcher.Launcher;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        ActivityHandler activityHandler = new ActivityHandler();
-        activityHandler.loadActivity(ActivityPaths.loginActivity, AssetPaths.title, AssetPaths.icon);
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Launcher.main(args);
     }
 }
